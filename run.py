f7c6deb275da94430e7b53ead08975d940a896d7
jorgenunez8526 <jorgenunez8526@gmail.com>
2024-10-14 10:42:00 -0400

    updated more scripts to finish the project

--- run.py
+++ run.py
@@ -12,7 +12,12 @@ for file in myFiles:
     myData = open(path + file)
     iData = myData.read().split('\n')
 
-    myDictionary = {"title";iData[0], "name";iData[1], "date";iData[2], "feedback";iData[3]}
+    myDictionary = {
+        "title": iData[0], 
+        "name": iData[1], 
+        "date": iData[2], 
+        "feedback": iData[3]
+        }
 
     response = requests.post(myLink, json=myDictionary)
 
